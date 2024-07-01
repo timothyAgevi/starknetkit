7893c8652a23425e6027c3a0b779a4eabf8ac8a4
timothyAgevi <timothyagevi@gmail.com>
2024-07-01 17:40:18 +0300

    add Tokenbound as default conector

--- src/helpers/defaultConnectors.ts
+++ src/helpers/defaultConnectors.ts
@@ -1,36 +1,35 @@
-import { type Connector } from "../connectors"
-import {
-  ArgentMobileConnector,
-  type ArgentMobileConnectorOptions,
-} from "../connectors/argentMobile"
-import { InjectedConnector } from "../connectors/injected"
-import { WebWalletConnector } from "../connectors/webwallet"
+import { Connector } from "../connectors";
+import { ArgentMobileConnector, type ArgentMobileConnectorOptions } from "../connectors/argentMobile";
+import { InjectedConnector } from "../connectors/injected";
+import { WebWalletConnector } from "../connectors/webwallet";
+import { TokenboundConnector } from "../connectors/TBAConnector";
 
 export const defaultConnectors = ({
   argentMobileOptions,
   webWalletUrl,
 }: {
-  argentMobileOptions?: ArgentMobileConnectorOptions
-  webWalletUrl?: string
+  argentMobileOptions?: ArgentMobileConnectorOptions;
+  webWalletUrl?: string;
 }): Connector[] => {
   const isSafari =
     typeof window !== "undefined"
       ? /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-      : false
+      : false;
 
-  const defaultConnectors: Connector[] = []
+  const defaultConnectors: Connector[] = [];
 
   if (!isSafari) {
     defaultConnectors.push(
       new InjectedConnector({ options: { id: "argentX" } }),
-    )
+    );
     defaultConnectors.push(
       new InjectedConnector({ options: { id: "braavos" } }),
-    )
+    );
   }
 
-  defaultConnectors.push(new ArgentMobileConnector(argentMobileOptions))
-  defaultConnectors.push(new WebWalletConnector({ url: webWalletUrl }))
+  defaultConnectors.push(new ArgentMobileConnector(argentMobileOptions));
+  defaultConnectors.push(new WebWalletConnector({ url: webWalletUrl }));
+  defaultConnectors.push(new TokenboundConnector({ tokenboundAddress: "", parentAccountId: "" }));
 
-  return defaultConnectors
+  return defaultConnectors;
 }
