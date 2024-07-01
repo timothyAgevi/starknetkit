f35a725162b2e27164004ffbd16a3e4386d02187
timothyAgevi <timothyagevi@gmail.com>
2024-07-01 17:41:59 +0300

    add Tokenbound as wallet on Modal that pops up

--- src/helpers/mapModalWallets.ts
+++ src/helpers/mapModalWallets.ts
@@ -1,15 +1,15 @@
-import { Connector } from "../connectors/connector"
-import { ARGENT_X_ICON } from "../connectors/injected/constants"
-import type { ModalWallet, StoreVersion } from "../window/modal"
-import { isString } from "lodash-es"
-import { WalletProvider } from "get-starknet-core"
-import type { StarknetWindowObject } from "starknet-types"
+import { Connector } from "../connectors/connector";
+import { ARGENT_X_ICON } from "../connectors/injected/constants";
+import type { ModalWallet, StoreVersion } from "../window/modal";
+import { isString } from "lodash-es";
+import { WalletProvider } from "get-starknet-core";
+import type { StarknetWindowObject } from "starknet-types";
 
 interface SetConnectorsExpandedParams {
-  availableConnectors: Connector[]
-  installedWallets: StarknetWindowObject[]
-  discoveryWallets: WalletProvider[]
-  storeVersion: StoreVersion | null
+  availableConnectors: Connector[];
+  installedWallets: StarknetWindowObject[];
+  discoveryWallets: WalletProvider[];
+  storeVersion: StoreVersion | null;
 }
 
 export const mapModalWallets = ({
@@ -20,64 +20,64 @@ export const mapModalWallets = ({
 }: SetConnectorsExpandedParams): ModalWallet[] => {
   const starknetMobile =
     window?.starknet_argentX as unknown as StarknetWindowObject & {
-      isInAppBrowser: boolean
-    }
+      isInAppBrowser: boolean;
+    };
 
-  const isInAppBrowser = starknetMobile?.isInAppBrowser
+  const isInAppBrowser = starknetMobile?.isInAppBrowser;
   if (isInAppBrowser) {
-    return []
+    return [];
   }
 
   const allInstalledWallets = installedWallets.map((w) =>
     availableConnectors.find((c) => c.id === w.id),
-  )
+  );
 
   const orderedByInstall = [
     ...availableConnectors.filter((c) => allInstalledWallets.includes(c)),
     ...availableConnectors.filter((c) => !allInstalledWallets.includes(c)),
-  ]
+  ];
 
   const connectors = orderedByInstall
     .map<ModalWallet | null>((c) => {
-      const installed = installedWallets.find((w) => w.id === c.id)
+      const installed = installedWallets.find((w) => w.id === c.id);
       if (installed) {
         const icon =
           installed.id === "argentX"
             ? { light: ARGENT_X_ICON, dark: ARGENT_X_ICON }
             : isString(installed.icon)
               ? { light: installed.icon, dark: installed.icon }
-              : installed.icon
+              : installed.icon;
 
         return {
           name: installed.name,
           id: installed.id,
           icon,
           connector: c,
-        }
+        };
       }
 
       const discovery = discoveryWallets
         .filter((w) =>
           Boolean(w.downloads[storeVersion as keyof typeof w.downloads]),
         )
-        .find((d) => d.id === c.id)
+        .find((d) => d.id === c.id);
 
       if (discovery) {
-        const { downloads } = discovery
+        const { downloads } = discovery;
 
         const discoveryIcon =
-          discovery.id === "argentX" ? ARGENT_X_ICON : discovery.icon
+          discovery.id === "argentX" ? ARGENT_X_ICON : discovery.icon;
         return {
           name: discovery.name,
           id: discovery.id,
           icon: { light: discoveryIcon, dark: discoveryIcon },
           connector: c,
           download: downloads[storeVersion as keyof typeof downloads],
-        }
+        };
       }
 
       if (!c || !c.id || !c.name) {
-        return null
+        return null;
       }
 
       return {
@@ -88,9 +88,9 @@ export const mapModalWallets = ({
         title: "title" in c && isString(c.title) ? c.title : undefined,
         subtitle:
           "subtitle" in c && isString(c.subtitle) ? c.subtitle : undefined,
-      }
+      };
     })
-    .filter((c): c is ModalWallet => c !== null)
+    .filter((c): c is ModalWallet => c !== null);
 
-  return connectors
+  return connectors;
 }
