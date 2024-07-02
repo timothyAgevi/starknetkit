03102999f4c51a8ff69715d5f3df387c8b34de8c
timothyAgevi <timothyagevi@gmail.com>
2024-07-03 02:01:30 +0300

    tokenbound aacount

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// src/index.ts in mystarknetkit package
+export * from './connectors';
+export * from './helpers';
+export * from './window';
+export { TokenboundConnector } from './connectors/TBAConnector/TBAConnector'; // Add this line
