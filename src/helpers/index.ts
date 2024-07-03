9383891950608946fb5ee5e00c18e73a2b332113
timothyAgevi <timothyagevi@gmail.com>
2024-07-03 12:32:31 +0300

    import * from helpers directory

---
+++ src/helpers/index.ts
@@ -0,0 +1,12 @@
+export * from './mapTargetUrlToNetworkId';
+export * from './defaultConnectors';
+export * from './getStarknetChainId';
+export * from './getStoreVersionFromBrowser';
+export * from './getWallets';
+export * from './lastConnected';
+export * from './mapModalWallets';
+export * from './publicRcpNodes';
+export * from './resetWalletConnect';
+
+
+
